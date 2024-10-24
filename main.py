9659c7f43b9c9288be1c72a8854d44db846664ee
Tleyna <101tleyna@gmail.com>
2024-10-24 23:20:28 +0300

    day 1 / 100 days

---
+++ main.py
@@ -0,0 +1,5 @@
+print("""Hey, I'm Lina
+October 24th 2024
+I am happy to start the 100 Days Python challenge! I will use Replit for this. 
+I feel joy because this is a great opportunity to learn new things and learn Python! I hope I can do it 🍄
+You can follow my progress at replit.com/@101tleyna""");
